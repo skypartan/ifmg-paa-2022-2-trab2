4cfbdbdbd1f0d5da3776688e2ea1d9ed8c9418a3
Lucas Guimarães Bernardes <lucasgb@skypartan.net>
2022-11-02 16:36:37 -0300

    wip

--- pc.py
+++ pc.py
@@ -14,32 +14,29 @@ def fits(capacity: int, objects: list[Thing], threshold: int) -> bool:
     # sum of objects weight <= capacity
     # sum of objects value >= threshold
 
-
-    return False
-
-
-
-def knapsackGreProc(self, W, V, M, n):
-    packs = []
-    for i in range(n): 
-        packs.append(KnapsackPackage(W[i], V[i]))
-    packs.sort(reverse = True)
-    
-    remain = M
+    remain = capacity
     result = 0
+
     i = 0
     stopProc = False
     while (stopProc != True):
-        if (packs[i].weight <= remain):
-            remain -= packs[i].weight;
-            result += packs[i].value;
-        print("Pack ", i, " - Weight ", packs[i].weight, " - Value ", packs[i].value)
-        if (packs[i].weight > remain):
+        if (objects[i].weight <= remain):
+            remain -= objects[i].weight
+            result += objects[i].value
+
+        print("Pack ", i, " - Weight ", objects[i].weight, " - Value ", objects[i].value)
+
+        if (objects[i].weight > remain):
             i += 1
-        if (i == n):
+
+        if (i == threshold):
+        # if result > threshold or i == len(objects):
             stopProc = True
+
     print("Max Value:\t", result)
 
+
+    return result >= threshold
 
 
 if __name__ == "__main__":
@@ -48,7 +45,7 @@ if __name__ == "__main__":
 
     things: list[Thing] = []
     for i in range(100):
-        things.append(Thing(random.randint(0, 10), i))
+        things.append(Thing(random.randint(1, 10), i))
 
-    print("backpack {", "capacity", "=>", capacity, "things", "=>", things "}")
+    print("backpack {", "capacity", "=>", capacity, "}")#, "things", "=>", things, "}")
     print("fits", "=>", fits(capacity, things, threshold))
